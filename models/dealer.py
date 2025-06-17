506fb3ac7352543053813b0a90edd99a8db33527
Tyreek <tarek.sabbagh07@gmail.com>
2025-06-17 14:16:31 +0200

    Player Funktionen

---
+++ models/dealer.py
@@ -0,0 +1,5 @@
+"""
+Der Dealer muss bei einem Punktwert 
+von 16 oder weniger weitere Karten ziehen. 
+Bei einem Punktwert von 17 oder mehr darf er nicht mehr ziehen.
+"""
--- models/player.py
+++ models/player.py
@@ -1,3 +1,4 @@
+# https://www.spielbanken-bayern.de/spielinfos/spielregeln/black-jack
 from models.hand import Hand
 
 class Player:
@@ -6,9 +7,41 @@ class Player:
         self.hand = Hand()
         self.geld = geld
         self.wette = 0
-        pass
 
     # Funktion für Wette platzieren
     # Funktion für Geld hinzufügen bzw abrechnen
     # Funktion für Name des Spielers mit Geld ausgeben __str__
     # 
+
+    # Spieler zieht Karte
+    def sayCard(self):
+        pass
+
+
+    # Spieler stoppt, wünscht keine weiteren Karten
+    def sayStopp(self):
+        pass
+
+    # Ergeben die ersten beiden Karten des Spielers einen Punktwert von neun, 
+    # zehn oder elf, so kann er seinen Einsatz verdoppeln, erhält aber nur noch eine Karte.
+    def sayDoubling(self):
+        pass
+
+    def placeBet(self):
+        pass
+
+    def winBet(self):
+        pass
+
+    def drawBet(self):
+        pass
+
+    def loseBet(self):
+        pass
+
+    def resetHand(self):
+        pass
+
+    def showHand():
+        pass
+
